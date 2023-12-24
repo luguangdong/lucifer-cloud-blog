672bd670157f8b8f781463b3ae22ad71bb6915f0
luguangdong <lgd15095370993@gmail.com>
2023-12-24 21:26:23 +0800

    optimize.

--- src/api/upload/index.ts
+++ src/api/upload/index.ts
@@ -10,46 +10,54 @@ interface beforeUploadRes extends Info {
     key: string
 }
 
-export const upload = async (params: { file_name: string; file: File, path?: string }): Promise<beforeUploadRes> => {
+
+
+export const upload = async (params: { file_name: string; file: File, path: string }): Promise<beforeUploadRes> => {
+    const fromData = new FormData()
+    fromData.append('file_name', params.file_name)
+    fromData.append('file', params.file)
+    fromData.append('path', params.path)
+
     let res = await http.request(
         {
             url: '/cos/upload',
-            method: 'get',
-            params: {
-                file_name: params.file_name,
-                path: params.path,
-            },
+            method: 'post',
+            data: fromData,
         },
         {
             isShowMessage: false,
+            serializeParams: false,
         },
     )
 
     const info = await imageInfo(params.file)
+    return ({...res.data, ...info});
 
-    const data = new FormData()
-    data.append('key', res.data.key)
-    data.append('Signature', res.data.authorization)
-    data.append('x-cos-security-token', res.data.token)
-    data.append('x-cos-meta-fileid', res.data.cos_file_id)
-    data.append('file', params.file)
-
-    return new Promise(async (resolve, reject) => {
-        await http.request(
-            {
-                url: res.data.url,
-                method: 'post',
-                data: data,
-            },
-            {
-                serializeParams: false,
-                isShowMessage: false,
-                isReturn: false,
-            },
-        )
-
-        resolve({...res.data, ...info})
-    })
+    // const info = await imageInfo(params.file)
+    //
+    // const data = new FormData()
+    // data.append('key', res.data.key)
+    // data.append('Signature', res.data.authorization)
+    // data.append('x-cos-security-token', res.data.token)
+    // data.append('x-cos-meta-fileid', res.data.cos_file_id)
+    // data.append('file', params.file)
+    //
+    // return new Promise(async (resolve, reject) => {
+    //     await http.request(
+    //         {
+    //             url: res.data.url,
+    //             method: 'post',
+    //             data: data,
+    //         },
+    //         {
+    //             serializeParams: false,
+    //             isShowMessage: false,
+    //             isReturn: false,
+    //         },
+    //     )
+    //
+    //     resolve({...res.data, ...info})
+    // })
 }
 
 export const delUpload = (params: { key: string }) => {
